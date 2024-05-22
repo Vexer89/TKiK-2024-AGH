6f41defedba16de4df08befd7d4dac9302aa1d40
Krzysztof Czerenko <krzysztofczerenko@gmail.com>
2024-05-22 22:17:54 +0200

    fixes

--- converter.py
+++ converter.py
@@ -13,24 +13,6 @@ class JtoPConverter(java_grammarVisitor):
             results.append(result)
         return "\n".join(results)
 
-
-
-def convert(input_text):
-    input_stream = InputStream(input_text)
-    lexer = java_grammarLexer(input_stream)
-    token_stream = CommonTokenStream(lexer)
-    parser = java_grammarParser(token_stream)
-    tree = parser.program()
-
-    converter = JtoPConverter()
-    result = converter.visit(tree)
-
-    return result
-
-    converter = MyConverter()
-
-    return result
-
 def visitImportDeclaration(self, ctx):
     import_name = ctx.extendedID().getText()
     return f"import {import_name}"
@@ -45,3 +27,15 @@ def visitClassDeclaration(self, ctx):
     interfaces = ", ".join(self.visit(interface) for interface in ctx.interfaces().ID()) if ctx.interfaces() else ""
     members = "\n".join(self.visit(member) for member in ctx.classBody().classMemberDeclaration())
     return f"class {class_name}{superclass}{interfaces}:\n{members}"
+
+def convert(input_text):
+    input_stream = InputStream(input_text)
+    lexer = java_grammarLexer(input_stream)
+    token_stream = CommonTokenStream(lexer)
+    parser = java_grammarParser(token_stream)
+    tree = parser.program()
+
+    converter = JtoPConverter()
+    result = converter.visit(tree)
+
+    return result
