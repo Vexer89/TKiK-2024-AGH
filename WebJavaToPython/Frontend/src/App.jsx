648e5f062d7b5689240a6a324888fcacd845fc01
Olaf Grykalowski <olaf.grykalowski@gmail.com>
2024-06-20 16:49:21 +0200

    fix: naming

--- WebJavaToPython/Frontend/src/App.jsx
+++ WebJavaToPython/Frontend/src/App.jsx
@@ -1,14 +1,14 @@
 
-import Translator from './Converter';
+import Converter from './Converter';
 
 function App() {
   return (
     <div className="App">
       <header className="bg-zinc-700  text-zinc-200 font-bold p-4">
-        <h1 className="text-xl">Java to Python Code Translator</h1>
+        <h1 className="text-xl">Java to Python Code Converter</h1>
       </header>
       <main className="p-4">
-        <Translator  />
+        <Converter  />
       </main>
     </div>
   );
